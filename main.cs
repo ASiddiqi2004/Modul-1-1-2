62c909596e01c48b3d9f688c3b606bc0c666f9ca
ASiddiqi2004 <amaansiddiqi@usf.edu>
2024-06-27 00:40:48 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+
+    Console.WriteLine ("Lyrics 1:");
+    Console.WriteLine ("Lyrics 2:");
+    Console.WriteLine ("Lyrics 3:");
+    Console.WriteLine ("Lyrics 4:");
+
+
+    
+  }
+
+ 
+}
